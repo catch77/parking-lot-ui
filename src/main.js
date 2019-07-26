ec0f45fdca4c26ed91bfabaac230ee914dedff14
Teng YANG <morenyang88@gmail.com>
2019-07-26 21:37:31 +0800

    Set up element ui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import element from 'element-ui';
+import './assets/element-ui.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(element);
 
 new Vue({
   router,
