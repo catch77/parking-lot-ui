9cfa991d1f7cd0424d3bf97ad78fc28f6d72dea5
zzw <kenzzw@163.com>
2019-07-28 11:23:49 +0800

    Update: router

--- src/router.js
+++ src/router.js
@@ -23,16 +23,27 @@ export default new Router({
     },
     {
       path: '/admin',
-      name: 'AdminConsole',
-      component: () => import('./layouts/AdminConsole.vue'),
+      name: 'admin',
+      component: () => import('./layouts/Admin.vue'),
       children: [
         {
-          path: '/admin/Dashboard',
-          component: () => import('./views/Dashboard/Dashboard.vue'),
-        },
-        {
-          path: '/admin/managePB',
-          component: () => import('./views/ManagePB/ManagePB.vue'),
+          path: '/admin/console',
+          name: 'AdminConsole',
+          component: () => import('./layouts/AdminConsole.vue'),
+          children: [
+            {
+              path: '/admin/console',
+              component: () => import('./views/Dashboard/Dashboard.vue'),
+            },
+            {
+              path: '/admin/console/Dashboard',
+              component: () => import('./views/Dashboard/Dashboard.vue'),
+            },
+            {
+              path: '/admin/console/managePB',
+              component: () => import('./views/ManagePB/ManagePB.vue'),
+            },
+          ],
         },
       ],
     },
