5a0c9533ff9ff549df51a8b7abfef63b9065f988
Teng YANG <morenyang88@gmail.com>
2019-07-28 22:56:35 +0800

    Update login logic

--- src/services/base.js
+++ src/services/base.js
@@ -25,12 +25,6 @@ instance.interceptors.response.use(
   },
   error => {
     const { response } = error;
-    if (response && response.status === 401) {
-      Notification.error({
-        title: '权限错误',
-        message: '权限错误或登录超时，需要重新登录',
-      });
-    }
     if (response && response.status >= 500) {
       Notification.error({
         title: '服务器错误!',
