981e88099f611a2b974ee4f1a73fd67054a97e3d
zzw <kenzzw@163.com>
2019-07-28 11:34:06 +0800

    Refactor: store of PB

--- src/store/pb.js
+++ src/store/pb.js
@@ -1,75 +1,28 @@
-const testData = [
-  {
-    id: '1',
-    name: '小一',
-    gender: '男',
-    address: '广东省珠海市',
-    joinTime: '2019-08-07',
-  },
-  {
-    id: '2',
-    name: '小二',
-    gender: '男',
-    address: '广东省珠海市',
-    joinTime: '2019-02-07',
-  },
-  {
-    id: '3',
-    name: '小三',
-    gender: '女',
-    address: '广东省珠海市',
-    joinTime: '2019-01-07',
-  },
-  {
-    id: '4',
-    name: '小四',
-    gender: '女',
-    address: '广东省珠海市',
-    joinTime: '2019-03-07',
-  },
-  {
-    id: '5',
-    name: '小五',
-    gender: '女',
-    address: '广东省珠海市',
-    joinTime: '2019-04-07',
-  },
-  {
-    id: '6',
-    name: '小六',
-    gender: '女',
-    address: '广东省珠海市',
-    joinTime: '2019-05-07',
-  },
-];
+import * as PbAPI from '../services/pb';
 
 const pbStore = {
   state: {
     pbList: [],
   },
   mutations: {
-    getBackPbList(state) {
-      ///getMapping
-      state.pbList = testData;
+    SET_PB_LIST(state, payload) {
+      state.pbList = payload;
     },
-    deletePb(state, payload) {
-      //deleteMapping
+    DELETE_PB(state, payload) {
       state.pbList.forEach(function(item, index) {
         if (item.id === payload) {
           state.pbList.splice(index, 1);
         }
       });
     },
-    updatePB(state, payload) {
-      //putMapping
+    UPDATE_PB(state, payload) {
       state.pbList.forEach(function(item, index) {
         if (item.id === payload.id) {
           state.pbList.splice(index, 1, payload);
         }
       });
     },
-    addPB(state, payload) {
-      //postMapping
+    ADD_PB(state, payload) {
       state.pbList.push(payload);
     },
   },
@@ -82,17 +35,25 @@ const pbStore = {
     },
   },
   actions: {
-    getBackPbList({ commit }) {
-      commit('getBackPbList');
+    fetchAllPb({ commit }) {
+      PbAPI.fetchAllPb().then(res => {
+        commit('SET_PB_LIST', res);
+      });
     },
     deletePb({ commit }, payload) {
-      commit('deletePb', payload);
+      PbAPI.deletePb(payload).then(() => {
+        commit('DELETE_PB', payload);
+      });
     },
     updatePB({ commit }, payload) {
-      commit('updatePB', payload);
+      PbAPI.updatePb(payload).then(() => {
+        commit('UPDATE_PB', payload);
+      });
     },
     addPB({ commit }, payload) {
-      commit('addPB', payload);
+      PbAPI.addPb(payload).then(() => {
+        commit('ADD_PB', payload);
+      });
     },
   },
 };
