74354a75a6238e5efbaffd6ffcb934d3bf1d9b5d
Teng YANG <morenyang88@gmail.com>
2019-07-28 20:29:14 +0800

    Update admin style

--- src/layouts/AdminConsole.vue
+++ src/layouts/AdminConsole.vue
@@ -4,16 +4,14 @@
       <admin-header></admin-header>
     </el-header>
     <el-container>
-      <el-aside width="250px">
+      <el-aside width="250px" class="admin-side">
         <admin-sidebar></admin-sidebar>
       </el-aside>
-      <el-main>
-        <div class="content-box">
-          <div class="content">
-            <router-view></router-view>
-          </div>
-          <el-footer>© {{ new Date().getFullYear() }} Ivy</el-footer>
+      <el-main class="admin-main">
+        <div class="admin-main-content">
+          <router-view></router-view>
         </div>
+        <el-footer>© {{ new Date().getFullYear() }} Ivy</el-footer>
       </el-main>
     </el-container>
   </el-container>
@@ -44,5 +42,39 @@ export default {
 
 .wrapper {
   min-height: 100vh;
+}
+
+.admin-main {
+  background: #f4f5f7;
+}
+
+.admin-side,
+.admin-main {
+  height: calc(100vh - 60px);
+  overflow: auto;
+}
+
+.admin-main {
+  display: flex;
+  flex-direction: column;
+}
+
+.admin-main-content {
+  flex: 1;
+}
+</style>
+
+<style lang="scss">
+.admin-container {
+  padding: 30px;
+  border: 1px solid #c1c7d0;
+  border-radius: 5px;
+  background: #fff;
+}
+
+.admin-pagination {
+  margin: 20px 0;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/views/ManagePB/PbEdit.vue
+++ src/views/ManagePB/PbEdit.vue
@@ -1,11 +1,5 @@
 <template>
-  <el-dialog
-    title="编辑"
-    :visible.sync="editVisible"
-    width="30%"
-    :show-close="false"
-    :close-on-press-escape="false"
-  >
+  <el-dialog title="编辑" :visible.sync="editVisible" width="30%" :show-close="false" :close-on-press-escape="false">
     <el-form ref="form" :model="form" label-width="100px">
       <el-form-item label="姓名">
         <el-input v-model="form.name"></el-input>
@@ -16,7 +10,7 @@
           <el-option label="FEMALE" value="FEMALE"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="入职时间" >
+      <el-form-item label="入职时间">
         <el-input v-model="form.joinTime" :disabled="true"></el-input>
       </el-form-item>
     </el-form>
