9d9f0cd5d58553a6dfa5f58ba696fde3a1b60baa
Teng YANG <teng.yang@oocl.com>
2019-07-29 23:26:11 +0800

    Update customer page style

--- src/views/Customer/customerSelect/CustomerSelect.vue
+++ src/views/Customer/customerSelect/CustomerSelect.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
- <el-row style="height:50%;margin:0 auto;text-align:center;left:20%; ">
-  <el-button type="success" plain @click="handleParking" style="width:8em;font-size:2em">我要停车</el-button><br>
-  <el-button type="success" plain @click="handleFetching" style="width:8em;font-size:2em" v-show="false">我要取车</el-button><br>
-    <el-button type="success" plain @click="handleLookingReceipt" style="width:8em;font-size:2em" v-show="false">查看我的订单</el-button>
+ <el-row style="padding-top:40px;text-align:center; ">
+  <el-button type="success" plain @click="handleParking" style="width:100%">我要停车</el-button><br>
+  <el-button type="success" plain @click="handleFetching" style="width:8em" v-show="false">我要取车</el-button><br>
+    <el-button type="success" plain @click="handleLookingReceipt" style="width:8em" v-show="false">查看我的订单</el-button>
 </el-row>
 </div>
 </template>
--- src/views/order/OrderConfirm.vue
+++ src/views/order/OrderConfirm.vue
@@ -1,55 +1,74 @@
 <template>
-<div style="margin:0px auto;font-size:2em">
- <el-form ref="form" :model="car" label-width="80px"  style="width:400px;margin-left:20%">
-  <label id="tipLabel" style="margin-left:25%;color:red;font-size:1em"></label>
-  <el-form-item label="车牌号">
-    <el-input v-model="car.carNo" style="width:230%px;"></el-input>
-  </el-form-item>
+  <div style="margin:0px auto;font-size:2em">
+    <el-form ref="form" :model="car" label-width="0" style="width:100%">
+      <label id="tipLabel" style="margin-left:25%;color:red;font-size:1em"></label>
+      <h3 class="confirm-title">请输入车牌号以继续</h3>
+      <el-form-item>
+        <el-input v-model="car.carNo"/>
+      </el-form-item>
 
-  <el-form-item size="large">
-    <el-button type="primary" @click="handleCommit"  style="margin-left:10%">确定</el-button>
-    <el-button style="margin-left:30%" @click="back">返回</el-button>
-  </el-form-item>
-</el-form>
-</div>
+    <div class="confirm-btn-bar">
+      <el-button style="margin-left:30%" @click="back">取消</el-button>
+      <el-button type="primary" @click="handleCommit" class="confirm-btn">确定 <i class="el-icon-right"></i></el-button>
+
+    </div>
+
+    </el-form>
+  </div>
 </template>
 <script>
-export default {
-  data: function() {
-    return {
-      car: {
-        carNo: '',
+  export default {
+    data: function() {
+      return {
+        car: {
+          carNo: '',
+        },
+        canclevisible: false,
+      };
+    },
+    props: {
+      addVisible: Boolean,
+    },
+    methods: {
+      handleCommit() {
+        var tipMessage = document.getElementById('tipLabel');
+        if (this.car.carNo === '') {
+          tipMessage.innerText = '车牌号不能为空';
+          return;
+        }
+        if (this.car.carNo.length !== 5) {
+          tipMessage.innerText = '车牌号长度不正确';
+          return;
+        }
+        this.$store
+          .dispatch('order/postCarNo', this.car.carNo)
+          .then(() => {
+            this.$message.success('添加成功');
+            this.$router.push('/customers/finishedorder');
+          })
+          .catch(() => {
+            this.$message.error('添加失败');
+          });
       },
-      canclevisible: false,
-    };
-  },
-  props: {
-    addVisible: Boolean,
-  },
-  methods: {
-    handleCommit() {
-      var tipMessage = document.getElementById('tipLabel');
-      if (this.car.carNo === '') {
-        tipMessage.innerText = '车牌号不能为空';
-        return;
-      }
-      if (this.car.carNo.length !== 5) {
-        tipMessage.innerText = '车牌号长度不正确';
-        return;
-      }
-      this.$store
-        .dispatch('order/postCarNo', this.car.carNo)
-        .then(() => {
-          this.$message.success('添加成功');
-          this.$router.push('/customers/finishedorder');
-        })
-        .catch(() => {
-          this.$message.error('添加失败');
-        });
+      back() {
+        this.$router.push('/customers/customerselect');
+      },
     },
-    back() {
-      this.$router.push('/customers/customerselect');
-    },
-  },
-};
+  };
 </script>
+
+<style lang="scss" scoped>
+  .confirm-title {
+    font-size: 32px;
+    font-weight: 400;
+    margin-bottom: 10px;
+  }
+
+  .confirm-btn-bar {
+    display: flex;
+    justify-content: flex-end;
+    .confirm-btn{
+width: 120px;
+    }
+  }
+</style>
